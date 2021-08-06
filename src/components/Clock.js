3f129aaa78d4ad4ae43d1ba0ec8be2d4d5213516
Magdalena Romanowska <magdalena.romanowska.poczta@gmail.com>
2021-08-06 11:09:11 +0200

    Week 04 Lesson 08 Homework

--- src/components/Clock.js
+++ src/components/Clock.js
@@ -2,8 +2,12 @@ import React from "react";
 
 function Clock({ className = "", minutes = 20, seconds = 48 }) {
   return (
-    <h2 className={"Clock " + className}>
-      Pozostało {minutes}:{seconds}
+    <h2 className={"clock " + className}>
+      Pozostało &nbsp; 
+      <div className={"clock__minutes"}>{minutes}</div>
+      <div className={"clock__separator"}>:</div>
+      <div className={"clock__seconds"}>{seconds}</div>
+      
     </h2>
   );
 }
--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -3,7 +3,7 @@ import React from "react";
 function ProgressBar({ className = "", percent = 33 }) {
   return (
     <div className={"progress progress--big progress--color-red" + className}>
-      <div className={"progress__bar"} style={{ width: `${percent}%` }}></div>
+      <div className={"progress__bar progress__bar--color-green"} style={{ width: `${percent}%` }}></div>
     </div>
   );
 }
